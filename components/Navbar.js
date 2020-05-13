fba16b6665d0a168f5d8d8ebfb55d4d1f34eaf6c
Ubuntu <joaomarcoslopes@id.uff.br>
2020-05-13 19:05:35 -0300

    feat: Navbar

---
+++ components/Navbar.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <header id="Navbar">
+      <img
+        id="logo"
+        src="/assets/bikcraft.svg"
+        alt="logo escrito o nome da marca 'bikcraft'"
+      />
+      <nav>
+        <ul>
+          <li>
+            <a href="/sobre">Sobre</a>
+          </li>
+          <li>
+            <a href="/produtos">Produtos</a>
+          </li>
+          <li>
+            <a href="/portfolio">Portfolio</a>
+          </li>
+          <li>
+            <a href="/contato">Contato</a>
+          </li>
+        </ul>
+      </nav>
+      <style jsx>{`
+        #Navbar {
+          background-color: var(--yellow);
+          padding: 10px 10px 10px 10px;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          min-heigth: 80px;
+          min-width: 50px;
+          width: 100%;
+        }
+
+        #Navbar nav ul {
+          margin-right: 30px;
+          display: flex;
+        }
+        #Navbar nav ul li {
+          padding-right: 20px;
+        }
+        #Navbar nav ul li a {
+          text-transform: uppercase;
+          color: black;
+          font-weight: bold;
+          text-decoration: none;
+        }
+      `}</style>
+    </header>
+  );
+}
